1b99018456d783933e514018e7214451ed3bf9a1
TheLexCraft <77213356+TheLexCraft@users.noreply.github.com>
2021-10-26 21:08:00 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,24 @@
+let frequenz = 0
 let zähler = 0
 let on__off = 1
+music.setVolume(150)
+loops.everyInterval(1000, function () {
+    if (on__off == 1) {
+        zähler = randint(1, 8)
+    }
+    frequenz = frequenz + 150
+})
 basic.forever(function () {
     if (on__off == 1) {
         if (zähler == 1) {
-            music.ringTone(zähler)
-        } else {
             on__off = 0
+        } else {
+            music.ringTone(frequenz)
         }
     }
 })
-loops.everyInterval(3000, function () {
-    zähler = randint(1, 5)
+basic.forever(function () {
+    if (on__off == 0) {
+        music.stopAllSounds()
+    }
 })
